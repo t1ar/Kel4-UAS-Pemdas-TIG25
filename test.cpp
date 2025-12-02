4347506bb127e332b91b381eff697a515bcd72ce
HelicopKun <szplay69@gmail.com>
2025-12-02 16:24:00 +0700

    removed fajri's nested-if

--- test.cpp
+++ test.cpp
@@ -8,9 +8,7 @@
 using namespace std;
 
 // declare harga & duit
-const int MAX = 9,
-          Harga_Buyer [MAX] = {},
-          Harga_Stock [MAX] = {};
+const int MAX = 9;
 int Wallet = 20000;    //buyer's wallet
 int Money, pilihan;   // Mr. Krabs : Money! | global var 
 string Nama_Item, Jumlah, Harga;
@@ -25,20 +23,18 @@ struct structItem {
 void BuyStock(int pilihan) {
     if (pilihan == 0) return; //in case of exit
     pilihan--; //due to how array works
-    if (Money < Harga_Stock[pilihan]){
+    if (Money < Storage[pilihan].Harga){
         cout << "Not enough money.\n\n";
         return;
     }
     
     Item[pilihan].Jumlah++;
-    Wallet -= Harga_Stock[pilihan];
+    Wallet -= Storage[pilihan].Harga;
     cout << "Restock completed.\nCurrent balance : " << Money;
 }
 
 // declare functions
-void Restock(int pilihan) 
-{ // specific number of restock
-    
+void Restock(int pilihan) { // specific number of restock
     if (pilihan == 0) return; //in case of exit
     pilihan--; //due to how array works
     cout << "Are you sure to restock " << Item[pilihan].Nama << "? (y/n) : ";
@@ -47,26 +43,24 @@ void Restock(int pilihan)
     if (confirm != 'y' && confirm != 'Y') {
         cout << "Restock cancelled.\n\n";
         return;
-    }
-    if (confirm == 'y' || confirm == 'Y')
-    {
+    } //fajri, the nested-if fangirl
+    if (Storage[pilihan].Nama != Item[pilihan].Nama) { //jri klo barangnya udah pasti sama, ga perlu di looping ber kali" dalam forloop
+        cout << "Not matching in warehouse.\n\n";
+        return;
+    }  
         for(int i = 0; i <= 5; i++) {
-            if (Storage[pilihan].Nama == Item[pilihan].Nama) {
-                if (Storage[pilihan].Jumlah == 0) {
-                    cout << "No stock in warehouse.\n\n";
-                    return;
-                }
-                Storage[pilihan].Jumlah--;
-                Item[pilihan].Jumlah++;
-                if (Item[pilihan].Jumlah == 5) {
-                    cout << "Vending machine full.\n\n";
-                    return;
-                }
+            if (Item[pilihan].Jumlah == 5) { //check first, add later. in case vending machinenya udah penuh dari awal
+                cout << "Vending machine full.\n\n";
+                break; //use break instead of return
+            }
+            if (Storage[pilihan].Jumlah == 0) { 
+                cout << "Out of stock.\n\n";
                 return;
             }
+            Storage[pilihan].Jumlah--;
+            Item[pilihan].Jumlah++;           
         }
         cout << "Restock completed.\n\n";
-    }
 }
 
 void Beli(int pilihan) { //buy 1 item
@@ -85,7 +79,7 @@ void Beli(int pilihan) { //buy 1 item
     cout << "Transaction completed.\nCurrent balance : " << Wallet;
 }
 
-void StockPilih(structItem arr[]){ //data is stored in struct not file remember, oh and at the bottom give the line "0. Exit" ;
+void StockView(structItem arr[], char confirm){ //data is stored in struct not file, also needs confirmation whether u want to display price or not
     cout << "\n=== STOCK ===\n";
     for (int l = 0; l < MAX; l += 3) {
         // print names with index
@@ -97,11 +91,17 @@ void StockPilih(structItem arr[]){ //data is stored in struct not file remember,
         for (int j = l; j < (l + 3); ++j) {
             cout << arr[j].Jumlah << " left\t\t\t";
         }
+        if (confirm == 'y' || confirm == 'Y') { // 'y' || 'Y' for display, else for no
+            cout << "\n";
+            for (int j = l; j < (l + 3); ++j) {
+                cout << arr[j].Harga << " left\t\t\t";
+            }
+        }
         cout << "\n\n";
     }
-                                               
+}                                        
 
-
+void Pilih() {
     cout << "\n0.Exit";
     do { //pilih
         cout << "\nPilih barang : ";
@@ -123,7 +123,8 @@ void Menu() {
         break;
     case 1: //buy
                     //give entry message
-        StockPilih(Item);
+        StockView(Item, 'y');
+        Pilih();
         Beli(pilihan);
         break;
     case 2: //view profit
@@ -132,13 +133,16 @@ void Menu() {
         break;
     case 3: //buystock
                 //give entry message
-        StockPilih(Storage);
+        StockView(Storage, 'y');
+        Pilih();
 
         break;
     case 4: //restock
         do {
                     //give entry message
-            StockPilih(Item);
+            StockView(Storage, 'n');
+            StockView(Item, 'n');
+            Pilih();
             Restock(pilihan);
         } while (pilihan != 0);
         break;
