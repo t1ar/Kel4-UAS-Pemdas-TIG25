038f3f3ca2414f3b98193751cf22a7b0aea51043
t1ar <80429306+t1ar@users.noreply.github.com>
2025-12-03 20:17:36 +0700

    Initial setup and comments for VendingMachine.cpp
    Re-write and clean up the test code for the VendingMachine.

---
+++ VendingMachine.cpp
@@ -0,0 +1,13 @@
+//Re-write and clean up the test code, this will be our final file
+//also:
+//(libary)
+//(global vars)
+//(function and struct declration)
+//(main)
+//(function and struct intialization)
+// we gonna make docs for each thing, so it will much better if we liveshare to do the rewrite -t1ar
+
+#include <iostream> //basic io for terminal
+#include <fstream> //file handling for write/read
+#include <string> // TODO : i forgot what string for, pls write 
+using namespace std;
